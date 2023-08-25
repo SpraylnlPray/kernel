7f27df2cf9919dba2fab81791faeaf96c52038f0
Spray|n|Pray <danielwildegger@gmx.de>
2023-08-25 21:00:46 +0200

    initial commit, print function

---
+++ src/boot/boot.asm
@@ -0,0 +1,132 @@
+ORG 0x7c00
+bits 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
+start:
+    jmp 0:step2
+
+step2:
+    cli ; Clear Interrupts
+    mov ax, 0x00
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov sp, 0x7c00
+    sti ; Enables Interrupts
+
+.load_protected:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+    jmp $
+
+; gtd
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:       ; CS should point to this
+    dw 0xffff   ; Segment limit first 0-15 bits
+    dw 0        ; Base first 0-15 bits
+    db 0        ; Base 16-23 bits
+    db 0x9a     ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:       ; DS, SS, ES, FS, GS
+    dw 0xffff   ; Segment limit first 0-15 bits
+    dw 0        ; Base first 0-15 bits
+    db 0        ; Base 16-23 bits
+    db 0x92     ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov eax, 1 ; starting sector, not 0 cause thats the boot sector
+    mov ecx, 100 ; total number of sectors
+    mov edi, 0x0100000 ; where to load them into
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+    mov ebx, eax ; backup the lba
+    ; send the highest 8 bits of the lba to hard disk controller
+    shr eax, 24
+    or eax, 0xe0 ; Select the master drive
+    mov dx, 0x1f6
+    out dx, al
+    ; Finished sending the highest 8 bits of the lba
+
+    ; Send the total sectors to read
+    mov eax, ecx
+    mov dx, 0x1f2
+    out dx, al
+    ; Finishe dsending the total sectors to read
+
+    ; Send more bits of the LBA
+    mov eax, ebx
+    mov dx, 0x1f3
+    out dx, al
+    ; Finsihed sending more bits of the LBA
+
+    ; Send more bits of the LBA
+    mov dx, 0x1f4
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 8
+    out dx, al
+    ; Finsihed sending more bits of the LBA
+
+    ; Send upper 16 bits of the LBA
+    mov dx, 0x1f5
+    mov eax, ebx ; Restore LBA
+    shr eax, 16
+    out dx, al
+    ; Finished sending upper 16 bits of the LBA
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Check if we need to read
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+; We need to read 256 Words at a time
+    mov ecx, 256 ; 256 words -> 512 bytes -> one sector
+    mov dx, 0x1f0
+    rep insw ; Stores 256-times from port in dx and stores it in location in edi (set above)
+    pop ecx
+    loop .next_sector ; Also decrements ecx
+    ; End of reading sectors into memory
+    ret
+
+
+times 510-($ - $$) db 0
+dw 0xAA55
---
+++ src/kernel.c
@@ -0,0 +1,79 @@
+#include "kernel.h"
+#include <stdint.h>
+#include <stddef.h>
+
+uint16_t *video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
+
+uint16_t terminal_make_char(char c, char color)
+{
+    // 0x0341 would print a green (or whatever) 'A' (hex code for ascii A is 41)
+    // Actually the color needs to be in first position
+    // but because of endiness they are flipped and the
+    // processor will turn it around.
+    // This adds the color, moves it to the left and then
+    // appends the character we want to print
+    return (color << 8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char color)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, char color)
+{
+    if (c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;
+    }
+
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col += 1;
+    if (terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
+}
+
+void terminal_init()
+{
+    video_mem = (uint16_t*)(0xB8000);
+    for (int y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (int x = 0; x < VGA_WIDTH; x++)
+        {
+            terminal_putchar(x, y, ' ', 0);
+        }
+    }
+}
+
+size_t strlen(const char *str)
+{
+    size_t len = 0;
+    while(str[len])
+    {
+        len++;
+    }
+
+    return len;
+}
+
+void print(const char *str)
+{
+    size_t len = strlen(str);
+    for (size_t i = 0; i < len; i++)
+    {
+        terminal_writechar(str[i], 15);
+    }
+}
+
+void kernel_main()
+{
+    terminal_init();
+    print("Hello world\n tessssst");
+}
