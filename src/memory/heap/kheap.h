ffb0de212ae00d929cc2e2def818b91a7e3196a9
Spray|n|Pray <danielwildegger@gmx.de>
2023-09-20 21:30:03 +0200

    Paging

--- src/memory/heap/kheap.h
+++ src/memory/heap/kheap.h
@@ -4,8 +4,9 @@
 #include <stdint.h>
 #include <stddef.h>
 
-void *kmalloc(size_t size);
+void* kmalloc(size_t size);
 void kheap_init();
 void kfree(void *ptr);
+void* kzalloc(size_t size);
 
 #endif
