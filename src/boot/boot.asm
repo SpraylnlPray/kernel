790517e2e3f2fb45bf090ca34ff74f38ebccaba8
Daniel Wildegger <danielwildegger@gmx.de>
2024-07-11 21:45:28 +0200

    FAT16 start

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -3,11 +3,32 @@ bits 16
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
-_start:
-    jmp short start
-    nop
 
-times 33 db 0
+jmp short start
+nop
+
+; FAT16 header
+OEMIdentifier               db 'DANOS   ' ; Must be 8 bytes, padding is done with spaces!
+BytesPerSector              dw 0x200 ; 512 bytes per sector, changing it doesn't necessarily mean the bps change, the way the disk works is fixed, generally ignored by most kernels
+SectorsPerCluster           db 0x80
+ReservedSectors             dw 200 ; Reserved for kernel, is plenty, whole kernel will be stored here, not stored in a file
+FATCopies                   db 0x02 ; Original + backup
+RootDirEntries              dw 0x40
+NumSectors                  dw 0x00
+MediaType                   db 0xF8
+SectorsPerFat               dw 0x100
+SectorsPerTrack             dw 0x20
+NumberOfHeads               dw 0x40
+HiddenSectors               dd 0x00
+SectorsBig                  dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber                 db 0x80
+WinNTBit                    db 0x00
+Signature                   db 0x29
+VolumeID                    dd 0xD105
+VolumeIDString              db 'DANOS BOOT ' ; Must be 11 bytes, padding is done with spaces!
+SystemIDString              db 'FAT16   ' ; Must be 8 bytes, padding is done with spaces!
 
 start:
     jmp 0:step2
