4f36c458534e35379c589d39bb2603a889491aab
Daniel Wildegger <danielwildegger@gmx.de>
2024-07-26 22:26:05 +0200

    fclose function

--- src/fs/file.h
+++ src/fs/file.h
@@ -40,6 +40,7 @@ typedef int (*FS_RESOLVE_FUNCTION)(struct disk* disk);
 typedef int (*FS_READ_FUNCTION)(struct disk* disk, void* privatee, uint32_t size, uint32_t nmemb, char* out);
 typedef int (*FS_SEEK_FUNCTION)(void* private, uint32_t offset, FILE_SEEK_MODE seek_mode);
 typedef int (*FS_STAT_FUNCTION)(struct disk* disk, void* private, struct file_stat* stat);
+typedef int (*FS_CLOSE_FUNCTION)(void* private);
 
 struct filesystem
 {
@@ -49,6 +50,7 @@ struct filesystem
     FS_READ_FUNCTION read;
     FS_SEEK_FUNCTION seek;
     FS_STAT_FUNCTION stat;
+    FS_CLOSE_FUNCTION close;
 
     char name[20];
 };
@@ -73,5 +75,6 @@ int fread(void* ptr, uint32_t size, uint32_t nmemb, int fd);
 int fstat(int fd, struct file_stat* stat);
 void fs_insert_filesystem(struct filesystem* filesystem);
 struct filesystem* fs_resolve(struct disk* disk);
+int fclose(int fd);
 
 #endif
