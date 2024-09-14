580355dc54678324d625064191b2b1e1893f1b37
Daniel Wildegger <danielwildegger@gmx.de>
2024-09-14 22:04:30 +0200

    elf-helper functions

---
+++ src/loader/formats/elfloader.h
@@ -0,0 +1,32 @@
+#ifndef ELFLOADER_H
+#define ELFLOADER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "elf.h"
+#include "config.h"
+
+struct elf_file
+{
+    char filename[DANOS_MAX_PATH];
+
+    int in_memory_size;
+
+    // pointer to physical memory address where this elf-file is loaded at
+    void * elf_memory;
+
+    // virtual base address of this binary
+    void * virtual_base_address;
+
+    // The ending virtual address
+    void * virtual_end_address;
+
+    // The physical base address
+    void* physical_base_address;
+
+    // Physical end address of this binary
+    void* physical_end_address;
+};
+
+#endif
