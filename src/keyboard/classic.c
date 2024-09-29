d8706c38236765d6506650440264034b3ecb9b45
Daniel Wildegger <danielwildegger@gmx.de>
2024-09-29 16:21:18 +0200

    capsloc and shift work with all chars

--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -7,22 +7,36 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define CLASSIC_KEYBOARD_CAPSLOCK 0x3A
-
 static uint8_t keyboard_scan_set_one[] = {
     0x00, 0x1B, '1', '2', '3', '4', '5',
     '6', '7', '8', '9', '0', '-', '=',
-    0x08, '\t', 'Q', 'W', 'E', 'R', 'T',
-    'Y', 'U', 'I', 'O', 'P', '[', ']',
-    0x0d, 0x00, 'A', 'S', 'D', 'F', 'G',
-    'H', 'J', 'K', 'L', ';', '\'', '`', 
-    0x00, '\\', 'Z', 'X', 'C', 'V', 'B',
-    'N', 'M', ',', '.', '/', 0x00, '*',
+    0x08, '\t', 'q', 'w', 'e', 'r', 't',
+    'y', 'u', 'i', 'o', 'p', '[', ']',
+    0x0d, 0x00, 'a', 's', 'd', 'f', 'g',
+    'h', 'j', 'k', 'l', ';', '\'', '`', 
+    0x00, '\\', 'z', 'x', 'c', 'v', 'b',
+    'n', 'm', ',', '.', '/', 0x00, '*',
     0x00, 0x20, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, '7', '8', '9', '-', '4', '5',
     '6', '+', '1', '2', '3', '0', '.'
 };
+
+static uint8_t keyboard_scan_set_one_shift[] = {
+    0x00, 0x1B, '!', '@', '#', '$', '%',
+    '^', '&', '*', '(', ')', '_', '+',
+    0x08, '\t', 'Q', 'W', 'E', 'R', 'T',
+    'Y', 'U', 'I', 'O', 'P', '{', '}',
+    0x0d, 0x00, 'A', 'S', 'D', 'F', 'G',
+    'H', 'J', 'K', 'L', ':', '"', '~', 
+    0x00, '|', 'Z', 'X', 'C', 'V', 'B',
+    'N', 'M', '<', '>', '?', 0x00, '*',
+    0x00, 0x20, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, '7', '8', '9', '-', '4', '5',
+    '6', '+', '1', '2', '3', '0', '.'
+};
+
 
 int classic_keyboard_init();
 void classic_keyboard_handle_interrupt();
@@ -41,43 +55,6 @@ int classic_keyboard_init()
     return 0;
 }
 
-uint8_t handle_capslock(char c)
-{
-    if (keyboard_get_capslock(&classic_keyboard) == KEYBOARD_CAPSLOCK_ON)
-    {
-        return c;
-    }
-
-    if (c >= 'A' && c <= 'Z')
-    {
-        c += 32;
-    }
-
-    return c;
-}
-
-uint8_t handle_shift(char c)
-{
-    if (keyboard_get_shift(&classic_keyboard) == KEYBOARD_SHIFT_OFF)
-    {
-        return c;
-    }
-
-    if (c >= 'A' && c <= 'Z')
-    {
-        c += 32;
-        return c;
-    }
-
-    if (c >= 'a' && c <= 'z')
-    {
-        c -= 32;
-        return c;
-    }
-
-    return c;
-}
-
 uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
 {
     size_t size_of_keyboard_set_one = sizeof(keyboard_scan_set_one) / sizeof(uint8_t);
@@ -87,8 +64,8 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
     }
 
     char c = keyboard_scan_set_one[scancode];
-    c = handle_capslock(c);
-    c = handle_shift(c);
+    if (keyboard_get_shift(&classic_keyboard) == KEYBOARD_SHIFT_ON || keyboard_get_capslock(&classic_keyboard) == KEYBOARD_CAPSLOCK_ON)
+        c = keyboard_scan_set_one_shift[scancode];
 
     return c;
 }
